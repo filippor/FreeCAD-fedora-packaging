a061596e90550685a2a91ca82cb77da0f74da4c4
filippor <filippo.rossoni@gmail.com>
2026-04-14 12:01:26 +0200

    unify script

--- scripts/create-archive.sh
+++ scripts/create-archive.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+rm -f freecad-sources.tar.gz
 git submodule update --init
+/usr/bin/python3 package/scripts/write_version_info.py freecad_version.txt
 bash -c 'git ls-files --recurse-submodules | tar  -caf freecad-sources.tar.gz -T-'
 echo -n 'freecad-sources.tar.gz'
--- scripts/post-upstream-clone.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-/usr/bin/python3 package/scripts/write_version_info.py freecad_version.txt
-rm -f freecad-sources.tar.gz
